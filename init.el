cc32ce726e86e4cc299e2c72877ae549d7a28383
Suraj Kumar <suraj.kumar.reddy@gmail.com>
2017-07-12 20:43:06 +0530

    Initial commit

---
+++ init.el
@@ -0,0 +1,18 @@
+(package-initialize)
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c5a886cc9044d8e6690a60f33db45506221aa0777a82ad1f7fe11a96d203fa44" default)))
+ '(package-selected-packages (quote (org-plus-contrib use-package)))
+ '(persp-keymap-prefix "w"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
