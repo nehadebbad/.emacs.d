fe1095a432be66872b7e5f02e7a95b31d376031f
Suraj Kumar <suraj.kumar.reddy@gmail.com>
2017-07-13 21:17:38 +0530

    solarized

--- custom.el
+++ custom.el
@@ -7,18 +7,8 @@
  '(company-quickhelp-delay 1.0)
  '(company-quickhelp-max-lines 10)
  '(company-quickhelp-use-propertized-text t)
- '(custom-safe-themes
-   (quote
-    ("c5a886cc9044d8e6690a60f33db45506221aa0777a82ad1f7fe11a96d203fa44" default)))
  '(ein:complete-on-dot nil)
  '(ein:notebook-modes (quote (ein:notebook-python-mode)))
  '(elpy-company-post-completion-function (quote elpy-company-post-complete-parens))
  '(package-selected-packages (quote (org-plus-contrib use-package)))
  '(persp-keymap-prefix "w"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(powerline-active1 ((t (:inherit mode-line :background "grey22" :foreground "white"))))
- '(powerline-active2 ((t (:inherit mode-line :background "grey40" :foreground "white")))))
