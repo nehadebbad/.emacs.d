9e82c462149e25675714eb6d461752bba06a9e21
Suraj Kumar <suraj.kumar.reddy@gmail.com>
2017-07-14 20:19:36 +0530

    Solarized light theme settings

--- custom.el
+++ custom.el
@@ -1,4 +1,3 @@
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -7,8 +6,34 @@
  '(company-quickhelp-delay 1.0)
  '(company-quickhelp-max-lines 10)
  '(company-quickhelp-use-propertized-text t)
+ '(custom-safe-themes
+   (quote
+    ("c5a886cc9044d8e6690a60f33db45506221aa0777a82ad1f7fe11a96d203fa44" default)))
  '(ein:complete-on-dot nil)
  '(ein:notebook-modes (quote (ein:notebook-python-mode)))
  '(elpy-company-post-completion-function (quote elpy-company-post-complete-parens))
+ '(frame-background-mode (quote dark))
  '(package-selected-packages (quote (org-plus-contrib use-package)))
  '(persp-keymap-prefix "w"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip ((t (:background "#eee8d5" :foreground "#6c71c4"))))
+ '(company-tooltip-annotation ((t (:background "#eee8d5" :foreground "#d33682"))))
+ '(company-tooltip-annotation-selection ((t (:inherit company-tooltip-annotation :background "dark orange" :foreground "black"))))
+ '(company-tooltip-common ((t (:background "#eee8d5" :foreground "#2aa198" :underline nil))))
+ '(company-tooltip-common-selection ((t (:background "dark orange" :foreground "black" :underline nil))))
+ '(company-tooltip-selection ((t (:background "dark orange" :foreground "black"))))
+ '(ivy-minibuffer-match-face-1 ((t (:background "navajo white"))))
+ '(ivy-minibuffer-match-face-2 ((t (:background "violet" :foreground "black" :weight bold))))
+ '(ivy-minibuffer-match-face-3 ((t (:background "LightSteelBlue1" :weight bold))))
+ '(ivy-minibuffer-match-face-4 ((t (:weight bold))))
+ '(mode-line ((t (:background "light gray" :foreground "black" :inverse-video nil :box nil))))
+ '(powerline-active1 ((t (:inherit mode-line :background "gray30" :foreground "white"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "dark gray" :foreground "black"))))
+ '(sp-pair-overlay-face ((t (:background "#fdf6e3"))))
+ '(spaceline-flycheck-error ((t (:foreground "red"))))
+ '(spaceline-flycheck-info ((t (:foreground "dark green"))))
+ '(spaceline-flycheck-warning ((t (:foreground "dark orange")))))
